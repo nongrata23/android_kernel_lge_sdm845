e7affb6e963c5601a51b110a6e99fef408010435
Ionela Voinescu <ionela.voinescu@arm.com>
2018-04-27 18:19:13 +0100

    ANDROID: sched/fair: unify spare capacity calculation
    Given that we have a few sites where the spare capacity of a CPU is
    calculated as the difference between the original capacity of the CPU
    and its computed new utilization, let's unify the calculation and use
    that value tracked with a local spare_cap variable.
    
    Change-Id: I78daece7543f78d4f74edbee5e9ceb62908af507
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Git-commit: 5383285d1358ea595a14436e6f14415fd0c1fa0e
    Git-repo: http://android.googlesource.com/kernel/common
    [adharmap@codeaurora.org: spare_cap is already used in 4.14
    baseline, rename that to spare_wake_cap to avoid name collision]
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7657,7 +7657,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			if ((capacity_orig - new_util) < target_max_spare_cap)
 				continue;
 
-			target_max_spare_cap = capacity_orig - new_util;
+			target_max_spare_cap = spare_cap;
 			target_capacity = capacity_orig;
 			target_util = new_util;
 			target_cpu = i;
