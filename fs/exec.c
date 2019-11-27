e909fcba9559b641731e92732557aa7888b0d34a
Adam W. Willis <return.of.octobot@gmail.com>
2019-11-27 18:14:34 -0500

    fs: exec: Introduce is_zygote_pid()
    Adapted from kerneltoast/android_kernel_google_bluecross@145e439, this is required for:
    
    "kernel: Boost whenever a zygote-forked process becomes a top app"
    "kernel: Boost to the max for a short amount of time when zygote forks"
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- fs/exec.c
+++ fs/exec.c
@@ -81,6 +81,17 @@ int suid_dumpable = 0;
 static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
+#define ZYGOTE32_BIN	"/system/bin/app_process32"
+#define ZYGOTE64_BIN	"/system/bin/app_process64"
+static atomic_t zygote32_pid;
+static atomic_t zygote64_pid;
+
+bool is_zygote_pid(pid_t pid)
+{
+	return atomic_read(&zygote32_pid) == pid ||
+		atomic_read(&zygote64_pid) == pid;
+}
+
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
 void __register_binfmt(struct linux_binfmt * fmt, int insert)
 {
@@ -1815,6 +1826,13 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (retval < 0)
 		goto out;
 
+	if (capable(CAP_SYS_ADMIN)) {
+		if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN)))
+			atomic_set(&zygote32_pid, current->pid);
+		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
+			atomic_set(&zygote64_pid, current->pid);
+	}
+
 	if (is_global_init(current->parent)) {
 		if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN_PREFIX,
@@ -1823,6 +1841,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}
 	}
+
 	/* execve succeeded */
 	current->fs->in_exec = 0;
 	current->in_execve = 0;
