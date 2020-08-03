95984a6f25acc93ee2a2da7138244cb78e858ebf
Emanuel <emanuelghub@gmail.com>
2020-08-03 23:13:53 +0400

    Revert "devfreq: Introduce devfreq boost driver"
    This reverts commit ccfc797f5a43c7da04c70fecfbe90ed1cd2adf83.

--- include/linux/devfreq.h
+++ include/linux/devfreq.h
@@ -183,8 +183,6 @@ struct devfreq {
 
 	unsigned long min_freq;
 	unsigned long max_freq;
-	bool is_boost_device;
-	bool max_boost;
 	bool stop_polling;
 
 	/* information for device frequency transition */
@@ -324,9 +322,6 @@ struct devfreq_passive_data {
 };
 #endif
 
-/* Caution: devfreq->lock must be locked before calling update_devfreq */
-extern int update_devfreq(struct devfreq *devfreq);
-
 #else /* !CONFIG_PM_DEVFREQ */
 static inline struct devfreq *devfreq_add_device(struct device *dev,
 					  struct devfreq_dev_profile *profile,
@@ -429,11 +424,6 @@ static inline struct devfreq *devfreq_get_devfreq_by_phandle(struct device *dev,
 }
 
 static inline int devfreq_update_stats(struct devfreq *df)
-{
-	return -EINVAL;
-}
-
-static inline int update_devfreq(struct devfreq *devfreq)
 {
 	return -EINVAL;
 }
