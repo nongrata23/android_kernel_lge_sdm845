28573092a882fe0273bb11c05b34ddf62f041341
Emanuel <emanuelghub@gmail.com>
2020-08-03 23:13:17 +0400

    Revert "devfreq_boost: Block max boosts while the screen is off"
    This reverts commit 35849dd24201f494347eaec496294b660202b928.

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -89,21 +89,12 @@ void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms)
 	if (!d)
 		return;
 
-	if (!is_display_on())
-		return;
-
 	__devfreq_boost_kick_max(d->devices + device, duration_ms);
 }
 
 void devfreq_boost_kick_wake(enum df_device device)
 {
-	struct df_boost_drv *d = df_boost_drv_g;
-
-	if (!d)
-		return;
-
-	__devfreq_boost_kick_max(d->devices + device,
-				 CONFIG_DEVFREQ_WAKE_BOOST_DURATION_MS);
+	devfreq_boost_kick_max(device, CONFIG_DEVFREQ_WAKE_BOOST_DURATION_MS);
 }
 
 void devfreq_register_boost_device(enum df_device device, struct devfreq *df)
