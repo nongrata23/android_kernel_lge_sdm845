dd233b1c5f1f88df84cc92fc60e51e20d435ba48
Emanuel <emanuelghub@gmail.com>
2020-08-03 23:12:22 +0400

    Revert "devreq_boost: Add llcc to boosting device"
    This reverts commit f2f77d5282b7bfc0465c609f0c47ad8854159266.

--- kernel/fork.c
+++ kernel/fork.c
@@ -2118,7 +2118,6 @@ long _do_fork(unsigned long clone_flags,
 	if (task_is_zygote(current)) {
 		cpu_input_boost_kick_max(32);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 32);
-		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 32);
 	}
 
 	/*
