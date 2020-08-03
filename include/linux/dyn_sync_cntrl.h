bad3cc4092f3ae32fc4a574812302ff3f3813c10
MadeOfGreat <ravenklawasd@gmail.com>
2020-08-03 12:13:54 +0530

    dynamic fsync: enable by default
    * also fix version no.
    
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>

--- include/linux/dyn_sync_cntrl.h
+++ include/linux/dyn_sync_cntrl.h
@@ -6,9 +6,9 @@
  * sdm845 changes by pappschlumpf (Erik Müller)
  */
 
-#define DYN_FSYNC_ACTIVE_DEFAULT false
+#define DYN_FSYNC_ACTIVE_DEFAULT true
 #define DYN_FSYNC_VERSION_MAJOR 2
-#define DYN_FSYNC_VERSION_MINOR 2
+#define DYN_FSYNC_VERSION_MINOR 3
 
 extern bool suspend_active;
 extern bool dyn_fsync_active;
