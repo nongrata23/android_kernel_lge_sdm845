6e14dbd5de7ce80eadfb48f03749d8c79ce4e491
Emanuel <emanuelghub@gmail.com>
2021-01-30 16:25:07 +0400

    kernel: armv8_deprecated: shut up about deprecated instructions

--- arch/arm64/kernel/armv8_deprecated.c
+++ arch/arm64/kernel/armv8_deprecated.c
@@ -505,7 +505,8 @@ static int cp15barrier_handler(struct pt_regs *regs, u32 instr)
 	}
 
 ret:
-	pr_warn_ratelimited("\"%s\" (%ld) uses deprecated CP15 Barrier instruction at 0x%llx\n",
+	if (0)
+		pr_warn_ratelimited("\"%s\" (%ld) uses deprecated CP15 Barrier instruction at 0x%llx\n",
 			current->comm, (unsigned long)current->pid, regs->pc);
 
 	regs->pc += 4;
