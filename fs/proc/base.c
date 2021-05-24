aa745144ba85fae4e88a18a77252c9b99fbf6a8d
Emanuel <emanuelghub@gmail.com>
2021-05-25 01:27:26 +0400

    Correct the id for the gcam we are using

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -1192,7 +1192,7 @@ err_unlock:
 	put_task_struct(task);
 	/* These apps burn through CPU in the background. Don't let them. */
 	if (!err && oom_adj >= 700) {
-		if (!strcmp(task_comm, "id.GoogleCamera")) {
+		if (!strcmp(task_comm, "id.GoogleCameraEng")) {
 			struct task_kill_info *kinfo;
 
 			kinfo = kmalloc(sizeof(*kinfo), GFP_KERNEL);
