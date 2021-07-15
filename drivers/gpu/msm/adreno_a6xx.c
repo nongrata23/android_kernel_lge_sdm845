655fc3973012326d0e52f672a61657954b7ad686
Emanuel <emanuelghub@gmail.com>
2021-07-15 13:16:42 +0400

    Fix errors

--- drivers/gpu/msm/adreno_a6xx.c
+++ drivers/gpu/msm/adreno_a6xx.c
@@ -3110,7 +3110,6 @@ static struct adreno_coresight a6xx_coresight_cx = {
 	.count = ARRAY_SIZE(a6xx_coresight_regs_cx),
 	.groups = a6xx_coresight_groups_cx,
 };
-#endif
 
 static struct adreno_perfcount_register a6xx_perfcounters_cp[] = {
 	{ KGSL_PERFCOUNTER_NOT_USED, 0, 0, A6XX_RBBM_PERFCTR_CP_0_LO,
